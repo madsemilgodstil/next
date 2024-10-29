3d27d6febebd274140c80e134e8fed9d9337345b
madsemilgodstil <mads93n2@stud.kea.dk>
2024-10-29 12:38:39 +0100

    dd

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,9 @@ import CookieConsent from './components/CookieConsent'
 export default function Home () {
   return (
     <div>
+      <h1 className='text-center text-[5rem] text-red-500'>
+        This is styled with tailwind!
+      </h1>
       <CookieConsent />
     </div>
   )
