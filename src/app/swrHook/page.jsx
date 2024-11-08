501de1d6c89226dbe1b898e9b086a25110bce609
madsemilgodstil <mads93n2@stud.kea.dk>
2024-11-08 11:37:54 +0100

    ddd

--- src/app/swrHook/page.jsx
+++ src/app/swrHook/page.jsx
@@ -2,14 +2,18 @@
 
 import useSWR from "swr";
 
-const fetcher = (url) => fetch(url).then((r) => r.json());
+const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export default function Page() {
   const { data, error } = useSWR(`https://dummyjson.com/products`, fetcher);
   if (error) return "Failed to load";
   if (!data) return "Loading...";
 
-  console.log(data.products);
-
-  return data.products[0].title;
+  return (
+    <ul>
+      {data.products.map((product) => {
+        return <li key={product.id}>{product.category}</li>;
+      })}
+    </ul>
+  );
 }
